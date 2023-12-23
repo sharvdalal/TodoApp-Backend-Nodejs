904281a54595b970d7edca0efbc555476647704b
Sharv Dalal <sharvdalal@gmail.com>
2023-12-23 15:55:08 +0530

    Initial Commit

---
+++ routes/user.js
@@ -0,0 +1,18 @@
+import express from "express"
+import { User } from "../models/user.js";
+import { getAllUsers, register, login, getMyProfile, logout} from "../controllers/user.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+
+const router = express.Router();
+
+router.get("/users/all",getAllUsers)
+
+router.post("/new", register)
+
+router.post("/login", login)
+
+router.get("/logout", logout)
+
+router.get("/me",isAuthenticated, getMyProfile) 
+
+export default router
